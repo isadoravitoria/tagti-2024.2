d58b6d5ed58c76eddf3df023b7171e71d2da3711
isadoravitoria <isadora.vitoria@aluno.ifsertao-pe.edu.br>
2024-10-21 20:59:21 -0300

    Utilização de render_tamblate

--- microblog/app.py
+++ microblog/app.py
@@ -1,13 +1,17 @@
 #python -m venv venv
 #.\venv\Scripts\activate
 #pip install flask
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá Mundo!'
+    return render_template("index.html")
+
+@app.route("/contato")
+def contato():
+    return "isadora.vitoria@aluno.ifsertao-pe.edu.br"
 
 if __name__ == '__main__':
     app.run()
