480bf03710616dbc54e92b3adcc09006e713bec8
isadoravitoria <isadora.vitoria@aluno.ifsertao-pe.edu.br>
2024-11-11 20:39:39 -0300

    curriculo

--- microblog/app.py
+++ microblog/app.py
@@ -13,13 +13,18 @@ def index():
 def contato():
     return render_template("contato.html", tel= "(87) 988445785",nome= "Isadora")
 
-@app.route("/user/<nome>", defauts={"sobrenome": "Nascimento"})
+
 @app.route("/user/<nome>/<sobrenome>")
 def user(nome,sobrenome):
     return f"Olá, {nome} {sobrenome}!"
 
 # Calculadora para somar dois números passados po parâmetro
-@app.route("/soma/<int:num1>/<int:num2>")
+#@app.route("/soma/<int:num1>/<int:num2>")
+
+#Minha pagina pessoal
+@app.route('/sobre')
+def sobre():
+    return render_template("sobre.html")
 
 if __name__ == '__main__':
     app.run()
